2b09257f8aaa888d2c768caa2d38e85b863576b3
Dawid <dcz31249@gmail.com>
2015-11-26 23:06:38 +0100

    add syntaxHighlight

--- src/utils.js
+++ src/utils.js
@@ -21,3 +21,33 @@ export const randInt = (min, max) => Math.floor(Math.random() * (max - min + 1))
  * @return {String} Color string
  */
 export const randCol = () => '#'+(Math.random()*0xFFFFFF<<0).toString(16); 
+
+
+/**
+ * turns a JSON string to html appending classes "string" and "string|number|boolean"
+ * for more info http://stackoverflow.com/questions/4810841/how-can-i-pretty-print-json-using-javascript
+ * @param {string} the string to format must be valid json
+ * @return {string} html to output on the page 
+ * 
+ */
+export function syntaxHighlight(json) {
+    if (typeof json != 'string') {
+         json = JSON.stringify(json, undefined, 2);
+    }
+    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+        var cls = 'number';
+        if (/^"/.test(match)) {
+            if (/:$/.test(match)) {
+                cls = 'key';
+            } else {
+                cls = 'string';
+            }
+        } else if (/true|false/.test(match)) {
+            cls = 'boolean';
+        } else if (/null/.test(match)) {
+            cls = 'null';
+        }
+        return '<span class="' + cls + '">' + match + '</span>';
+    });
+}
