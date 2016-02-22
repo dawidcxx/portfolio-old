6fe3ca99e68e112e64901d93f6fc3b1ff9e063f5
Dawid <dcz31249@gmail.com>
2016-02-22 10:03:05 +0100

    polyfill CSS objectFit for older browsers

--- src/js/landingbg.js
+++ src/js/landingbg.js
@@ -1,4 +1,5 @@
 import starify from './starify';
+import {RAF} from './RAF';
 
 var width = window.innerWidth;
 var height = (window.innerHeight > 600) ? window.innerHeight : 600;
@@ -18,9 +19,36 @@ videoEl.loop = true;
 videoEl.style.background = '#333';
 videoEl.style.objectFit = 'fill';
 
-mountEl.appendChild(videoEl);
+if('objectFit' in document.body.style) {
+  
+  mountEl.appendChild(videoEl);
 
-// videoEl.play();
+} else { // for IE and other browsers needing some help to streech a video.
+
+  let projectingCanvasEl = document.createElement('canvas');
+  projectingCanvasEl.width = width;
+  projectingCanvasEl.height = height;
+  let ctx = projectingCanvasEl.getContext('2d');
+  let video = null;
+
+  let render = () => {
+    ctx.drawImage(video, 0, 0, width, height);
+    RAF(render);
+  };
+
+  videoEl.addEventListener('play', function (_) {
+    video = this;
+    render();
+  })
+
+  videoEl.style.display = 'none';
+  document.body.appendChild(videoEl);
+
+  mountEl.appendChild(projectingCanvasEl);
+
+}
+
+videoEl.play();
 
 
 starify(mountEl, width, height - 150, 6, false);
